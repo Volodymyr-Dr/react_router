74ea62d14b15a74a6020d48b7550ef0205f52173
Volodymyr-Dr <vv.dr.pvlk@gmail.com>
2021-08-27 19:23:59 +0300

    add 404

--- src/NotFound/NotFound.js
+++ src/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => (
   <div>
     <h1>404 - Not Found!</h1>
-    <Link to="/">
+    <Link to="/react_router/">
       Go Home
     </Link>
   </div>
